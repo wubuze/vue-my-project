67596d89184afa58a87919060ef4256807e88e76
wubuze <1039289613@qq.com>
2018-02-04 21:49:01 +0800

    up

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,27 +2,43 @@
   <div id="index">
     <h1>welcome</h1>
     <h2> {{data2}} </h2>
+    <h2> {{todos}} </h2>
+    <h2> + {{count2}} </h2>
+    <button @click="setIncre"> + {{count2}} </button>
   </div>
 </template>
 
 <script>
   import * as rootController from '@/api/rootController'
+  import { mapState, mapGetters, mapMutations } from 'vuex'
 
   export default {
     data() {
       return {
-        data2: []
+        data2: [],
+        count2: 0
       }
+    },
+    computed: {
+      ...mapState(['orderList', 'login', 'todos', 'count']),
+      ...mapGetters(['doneTodosCount'])
     },
     created() {
       this.getTest()
+//      console.log(this.orderList)
     },
     methods: {
       getTest() {
-        rootController.test().then((res) => {
-//          console.log(res)
-          this.data2 = res
-        })
+//        rootController.test().then((res) => {
+////          console.log(res)
+////          this.data2 = res,
+//        })
+        this.data2 = this.doneTodosCount
+      },
+      ...mapMutations(['increment']),
+      setIncre() {
+        this.increment()
+        console.log(this.count)
       }
     }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -6,9 +6,29 @@ import vuex from 'vuex'
 Vue.use(vuex);
 
 const state = {
-    show: false
-}
+  show: false,
+  orderList: [1,2,4],
+  login: false,
+  count: 0,
+    todos: [
+        { id: 1, text: '...', done: true },
+        { id: 2, text: '...', done: false }
+    ]
+};
+const getters = {
+    doneTodosCount: (state) => {
+        return state.todos.filter(todo => todo.done)
+    }
+};
+const mutations = {
+    increment: (state) => {
+        return state.count ++
+    }
+};
+
 
 export default new vuex.Store({
-  state
+  state,
+  getters,
+  mutations
 })
